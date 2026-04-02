570ab845c893a6f66e85a22be0f8478a703e293c
DaienM <77191072+DaienM@users.noreply.github.com>
2026-04-02 19:26:04 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -29,6 +29,6 @@ CREATE TABLE Order_Items (
   order_id INT NOT NULL,
   product_id INT NOT NULL,
   quantity INT NOT NULL,
-  FOREIGN KEY (order_id) REFERENCES Orders(order.id),
+  FOREIGN KEY (order_id) REFERENCES Orders(order_id),
   FOREIGN KEY (product_id) REFERENCES Products(product_id)
 );
