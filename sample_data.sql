4227d76b164eb9c07103c60604c2d59aa0ae49b9
DaienM <77191072+DaienM@users.noreply.github.com>
2026-04-28 14:25:08 +0300

    Update sample_data.sql

--- sample_data.sql
+++ sample_data.sql
@@ -13,14 +13,14 @@ INSERT INTO Products (product_id, product_name, price, stock) VALUES
 (3, 'Keyboard', 45.00, 30);
 
 -- Orders
-INSERT INTO Orders (order_id, user_id, order_date) VALUES
-(1, 1, '2026-02-01'),
-(2, 2, '2026-02-02'),
-(3, 1, '2026-02-02');
+INSERT INTO Orders (order_id, user_id,total, order_date) VALUES
+(1, 1, 1250.00, '2026-02-01'),
+(2, 2, 46.00, '2026-02-02'),
+(3, 1, 78.00, '2026-02-02');
 
 -- Order_Items
-INSERT INTO Order_Items (order_item_id, order_id, product_id, quantity) VALUES
-(1, 1, 1, 1),
-(2, 1, 2, 2),
-(3, 2, 3, 1),
-(4, 3, 2, 3);
+INSERT INTO Order_Items (order_item_id, order_id, product_id, quantity, price) VALUES
+(1, 1, 1, 1, 1200.00),
+(2, 1, 2, 2, 25.50),
+(3, 2, 3, 1, 45.00),
+(4, 3, 2, 3, 25.50);
